461addfbcc08ae1632134f558e41436c6de38f28
tielixir <bluecluey6407@gmail.com>
2026-02-16 17:51:52 +0530

    fix: hardcode API_URL to /api for production

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' ? "/api" : "http://127.0.0.1:8000");
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "/api";
 
 export const api = {
     auth: {
